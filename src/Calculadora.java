b92867d81212553dbdc7befa27e69b3ff3514b45
Manuelpg05 <manuel.p.g.21.24@gmail.com>
2024-04-19 09:27:12 +0200

    Funcion 'restar' creada

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,6 +1,23 @@
 
 public class Calculadora {
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 		
+	}
+		
+	public double restar(double[] numeros) {
+		double resultado = 0;
+		boolean primeraLinea = true;
+		
+		for (double numero : numeros) {
+			if (primeraLinea) {
+				resultado = numero;
+				primeraLinea = false;
+			}
+			else {
+				resultado -= numero;
+			}
+		}
+		
+		return resultado;
 	}
 }
